f1123b6cec2ae96df12061e6396a70eb75c0ef2b
CAscencio <ciurlizza.ascencio@vallegrande.edu,pe>
2021-06-13 14:55:46 -0500

    first commit

---
+++ src/main/java/com/ascencio/dev/HandlingExceptionSpringWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.ascencio.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandlingExceptionSpringWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandlingExceptionSpringWebfluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ascencio/dev/HandlingExceptionSpringWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ascencio.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandlingExceptionSpringWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
